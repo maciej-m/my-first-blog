9d1ebd6c315616b27ffce8819ca9a32003c90ada
maciej-m <maciej.makarczuk@gmail.com>
2017-01-30 11:46:53 +0100

    naprawa formularza

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,11 +11,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-	
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-	
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -27,7 +24,10 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-	
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
