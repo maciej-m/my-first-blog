a547005858130d0094d0ca75af555e5a905055f2
maciej-m <maciej.makarczuk@gmail.com>
2017-02-09 14:52:00 +0100

    aktualizacja ćwiczeń

--- cwiczenia/kolejny dwa po imporcie.py
+++ cwiczenia/kolejny dwa po imporcie.py
@@ -4,4 +4,4 @@ from kolejny import hello_user
 
 name = ("PRZYWITAJ SIĘ Z KOPIĄ! ") # jak zrobić, by była wyświetlana wartość z kopii?
 
-hello_user()
+hello_user(name,surname)
--- cwiczenia/kolejny.py
+++ cwiczenia/kolejny.py
@@ -1,38 +1,33 @@
+# zrobić z tego funkcję, która:
+# po poprawnym wykoanniu funkcji, osoba zotaje zapisana na liście gości na blogu
+# dodatkowo, niech np. dodaje jakiś randomowy przydomek ze słownika (trza utworzyć)
+#
+
 def hello_user():
     "Powinien wyświetlać to, co sobie user wpisze jako string"
 
     name = input("Your name is: ")
     surname = input("Your surname is: ")
 
-    if name == "" and surname == "":
-        input ("You didn't tell your name and surname! Want to try again? Y/N ")
-
-        if input == "y" or "Y": #jak zrobić by ten warunek zadziałał?
-            print ("OK. TRYING AGAIN")
-            hello_user()
-
-        else:
-            print ("BYE!")
-
+    if name == "" and surname == "": # zrobić z tego funkcję dla wszystkich przypadków
+        print ("You didn't tell your name and surname! Want to try again? Y/N")
+        no_name()
     elif name == "":
-        input ("Tell your name! Want to try again? Y/N ")
-        if "y" or "Y":
-            print ("OK. TRYING AGAIN")
-            hello_user()
-        else:
-            print ("BYE!")
-
+        print ("You didn't tell your name! Want to try again? Y/N ")
+        no_name()
     elif surname == "":
-        input ("Tell your surname! Want to try again? Y/N ")
-        if "y" or "Y":
-            print ("OK. TRYING AGAIN")
-            hello_user()
-        else:
-            print ("BYE!")
-
+        no_name_surname = input ("You didn't tell your surname! Want to try again? Y/N ")
+        no_name()
     else:
-        print  ("Hello, " + name + " " + surname + "!")
+        print ("Hello, " + name + " " + surname + "!")
 
-
+def no_name(): #jak zaimplementować tę funkcję do hello_user?
+    no_name_surname = input()
+    no_name_surname = no_name_surname.upper()
+    if no_name_surname == "Y":
+        print ("OK. TRYING AGAIN")
+        hello_user()
+    else:
+        print ("BYE THEN!")
 
 hello_user()
